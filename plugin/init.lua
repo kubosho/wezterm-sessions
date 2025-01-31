2d17a1cd802c1b85b0ca8ae1781090fa767608fb
abidibo <abidibo@gmail.com>
2025-01-31 08:56:45 +0100

    refactor: var names and comments in init

--- plugin/init.lua
+++ plugin/init.lua
@@ -4,6 +4,7 @@ local act = wezterm.action
 ---@class public_module
 local pub = {}
 
+--- wezterm plugin directory
 local plugin_dir
 
 --- checks if the user is on windows
@@ -25,7 +26,7 @@ local function get_require_path()
     return directory_exists(path2) and path2 or path1
 end
 
---- adds the wezterm plugin directory to the lua path
+--- Adds the wezterm plugin directory to the lua path
 local function enable_sub_modules()
     plugin_dir = wezterm.plugin.list()[1].plugin_dir:gsub(separator .. "[^" .. separator .. "]*$", "")
     package.path = package.path
@@ -38,29 +39,29 @@ local function enable_sub_modules()
         .. separator
         .. "?.lua"
 end
-
 enable_sub_modules()
 
-local ws = require("workspace")
-local fs = require("fs")
+--- Now we can import local stuff
+local ws_mod = require("workspace")
+local fs_mod = require("fs")
 
--- The directory where we store the workspaces state
+--- The directory where we store the workspaces state
 local save_state_dir = plugin_dir .. separator .. get_require_path() .. separator .. "state" .. separator
 
 --- Loads the saved json file matching the current workspace.
 function pub.restore_state(window)
     local workspace_name = window:active_workspace()
-    ws.restore_workspace(window, save_state_dir, workspace_name)
+    ws_mod.restore_workspace(window, save_state_dir, workspace_name)
 end
 
 
 --- Allows to select which workspace to load
 function pub.load_state(window, pane)
-    local choices = ws.get_workspaces(save_state_dir)
+    local choices = ws_mod.get_workspaces(save_state_dir)
 
     window:perform_action(
         act.InputSelector({
-            action = wezterm.action_callback(function(inner_window, inner_pane, id, label)
+            action = wezterm.action_callback(function(_, inner_pane, id, label)
                 if id and label then
                     wezterm.log_info("Switching to ws: " .. label)
                     -- switch to workspace
@@ -70,6 +71,7 @@ function pub.load_state(window, pane)
                         },
                         inner_pane
                     )
+                    -- we need to wait for the switch to complete
                     wezterm.sleep_ms(2000)
                     window:perform_action(
                         act.EmitEvent 'wezter-sessions-switch',
@@ -87,20 +89,21 @@ function pub.load_state(window, pane)
     )
 end
 
-wezterm.on("wezter-sessions-switch", function(window, pane)
+--- After the workspace switch is complete we restore the workspace
+wezterm.on("wezter-sessions-switch", function(window, _)
     pub.restore_state(window)
 end)
 
 --- Orchestrator function to save the current workspace state.
 -- Collects workspace data, saves it to a JSON file, and displays a notification.
 function pub.save_state(window)
-    local data = ws.retrieve_workspace_data(window)
+    local data = ws_mod.retrieve_workspace_data(window)
 
     -- Construct the file path based on the workspace name
-    local file_path = save_state_dir .. "wezterm_state_" .. fs.escape_file_name(data.name) .. ".json"
+    local file_path = save_state_dir .. "wezterm_state_" .. fs_mod.escape_file_name(data.name) .. ".json"
 
     -- Save the workspace data to a JSON file and display the appropriate notification
-    if fs.save_to_json_file(data, file_path) then
+    if fs_mod.save_to_json_file(data, file_path) then
         window:toast_notification('WezTerm Sessions', 'Workspace state saved successfully', nil, 4000)
     else
         window:toast_notification('WezTerm Sessions', 'Failed to save workspace state', nil, 4000)
@@ -109,15 +112,15 @@ end
 
 --- Allows to select which workspace to delete
 function pub.delete_state(window, pane)
-    local choices = ws.get_workspaces(save_state_dir)
+    local choices = ws_mod.get_workspaces(save_state_dir)
 
     window:perform_action(
         act.InputSelector({
             action = wezterm.action_callback(function(inner_window, inner_pane, id, label)
                 if id and label then
                     wezterm.log_info("Deleting ws: " .. label)
-                    local file_path = save_state_dir .. "wezterm_state_" .. fs.escape_file_name(label) .. ".json"
-                    if fs.delete_json_file(file_path) then
+                    local file_path = save_state_dir .. "wezterm_state_" .. fs_mod.escape_file_name(label) .. ".json"
+                    if fs_mod.delete_json_file(file_path) then
                         window:toast_notification('WezTerm Sessions', 'Workspace state deleted successfully', nil, 4000)
                     else
                         window:toast_notification('WezTerm Sessions', 'Failed to delete workspace state', nil, 4000)
@@ -134,7 +137,7 @@ function pub.delete_state(window, pane)
     )
 end
 
----sets default keybindings
+---Sets default keybindings
 function pub.apply_to_config(config)
     if config == nil then
         config = {}
@@ -166,6 +169,7 @@ function pub.apply_to_config(config)
     })
 end
 
+--- Event handlers
 wezterm.on("save_session", function(window) pub.save_state(window) end)
 wezterm.on("load_session", function(window, pane) pub.load_state(window, pane) end)
 wezterm.on("restore_session", function(window) pub.restore_state(window) end)
